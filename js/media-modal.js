53d2045374c72216ee6ed2f41e052977a44ac983
Gary Smirny <banderon@gmail.com>
2013-05-15 09:48:56 -0400

    cleaned up documentation

--- js/media-modal.js
+++ js/media-modal.js
@@ -1,66 +1,72 @@
 /***
  *
  * MediaModal - Create a WP 3.5+ modal window for media selection
- * 
- ***
  *
- * Example with multiple PDFs:
- *
- * <a id="set-pdf" data-attachment_ids="[34,35,36]" data-uploader_title="Select Report" href="#">Click here to attach reports</a>
- * <input type="hidden" id="report-ids" name="report-ids" value="" />
- *
- * <script>
- * var mm = new MediaModal(
- *   {
- *     calling_selector : "#set-pdf",
- *     cb : function(attachments){
- *       var ids = jQuery.map(attachments, function(attachment){
- *         return attachment.id;
- *       });
- *       ids = JSON.stringify(ids);
- *       jQuery('#set-pdf').data('attachment_ids', ids);
- *       jQuery('#report-id').val(ids);
- *     }
- *   },
- *   {
- *     multiple: true,
- *     library : {
- *       type : "application/pdf"
- *     }
- *   }
- * );
- * </script>
+ * @module MediaModal
+ * @requires jQuery
+ * @author Chris Scott
+ * @author Gary Smirny
  *
  ***
  *
- * Example with single image:
+ * @usage Example with multiple PDFs:
  *
- * <a id="set-image" data-attachment_ids="12" href="#">Choose an image</a>
- * <img id="my-image" src="" />
- * <input type="hidden" id="image-id" name="image-id" value="" />
+ *   <a id="set-pdf" href="#" data-uploader_title="Select Report" 
+ *     data-attachment_ids="[34,35,36]">Click here to attach reports</a>
+ *   <input type="hidden" id="report-ids" name="report-ids" value="" />
  *
- * <script>
- * var mm = new MediaModal(
- *   {
- *     calling_selector : "#set-image",
- *     cb : function(attachments){
- *       var attachment = attachments[0];
- *       jQuery('#set-image').data('attachment_ids', attachment.id);
- *       jQuery('#my-image').attr('src', attachment.sizes.full.url);
- *       jQuery('#image-id').val(attachment.id);
- *     }
- *   },
- *   {
- *     title : 'Choose an Image',
- *     button : {
- *       text : 'Select Image'
+ *   <script>
+ *   var mm = new MediaModal(
+ *     {
+ *       calling_selector : "#set-pdf",
+ *       cb : function(attachments){
+ *         var ids = jQuery.map(attachments, function(attachment){
+ *           return attachment.id;
+ *         });
+ *         ids = JSON.stringify(ids);
+ *         jQuery('#set-pdf').data('attachment_ids', ids);
+ *         jQuery('#report-id').val(ids);
+ *       }
  *     },
- *     library : {
- *       type : "image"
+ *     {
+ *       multiple: true,
+ *       library : {
+ *         type : "application/pdf"
+ *       }
  *     }
- *   }
- * );
- * </script>
+ *   );
+ *   </script>
+ *
+ ***
+ *
+ * @usage Example with single image:
+ *
+ *   <a id="set-image" data-attachment_ids="12" href="#">Choose an image</a>
+ *   <img id="my-image" src="" />
+ *   <input type="hidden" id="image-id" name="image-id" value="" />
+ *
+ *   <script>
+ *   var mm = new MediaModal(
+ *     {
+ *       calling_selector : "#set-image",
+ *       cb : function(attachments){
+ *         var attachment = attachments[0];
+ *         jQuery('#set-image').data('attachment_ids', attachment.id);
+ *         jQuery('#my-image').attr('src', attachment.sizes.full.url);
+ *         jQuery('#image-id').val(attachment.id);
+ *       }
+ *     },
+ *     {
+ *       title : 'Choose an Image',
+ *       button : {
+ *         text : 'Select Image'
+ *       },
+ *       library : {
+ *         type : "image"
+ *       }
+ *     }
+ *   );
+ *   </script>
  *
  ***
  *
@@ -70,17 +76,28 @@
  *
  * @param {Object} settings  Settings for this class
  *   @param {String} calling_selector  The selector that is calling the modal
- *   @param {Function} cb  Callback function for processing the selected media item; accepts attachment object as argument
- * @param {Object} [render_options]  Rendering options to pass to the modal; specifiying any option will override the default for that option
- *   @param {String} [render_options.title]  Title to display at the top of the modal; defaults to selector's 'uploader_title' data field ('data-uploader_title' attribute)
+ *   @param {Function} cb  Callback function for processing the selected media
+ *   item; accepts attachment object as argument
+ * @param {Object} [render_options]  Rendering options to pass to the modal;
+ * specifiying any option will override the default for that option
+ *   @param {String} [render_options.title]  Title to display at the top of the
+ *   modal; defaults to selector's 'uploader_title' data field
+ *   ('data-uploader_title' attribute)
  *   @param {Object} [render_options.button]
- *	   @param {String} [render_options.button.text]  Text to display in submission button; defaults to selector's 'uploader_button_text' data field ('data-uploader_button_text' attribute)
- *	 @param {Bool} [render_options.multiple=false]  Whether users may select more than one item
- *	 @param {Object} [render_options.library]
- *	   @param {String} [render_options.library.type]  Only dispaly items of a specific MIME type (Ex: 'image', 'image/png', 'application/pdf')
- *     @param {Int} [render_options.library.uploadedTo]  Only display items uploaded to a specific post ID
- *     @param {String} [render_options.library.orderby='date']  Set the orderby for the items displayed
- *     @param {String} [render_options.library.order='DESC']  Set the order of the items displayed
+ *     @param {String} [render_options.button.text]  Text to display in
+ *     submission button; defaults to selector's 'uploader_button_text' data
+ *     field ('data-uploader_button_text' attribute)
+ *   @param {Bool} [render_options.multiple=false]  Whether users may select
+ *   more than one item
+ *   @param {Object} [render_options.library]
+ *     @param {String} [render_options.library.type]  Only dispaly items of a
+ *     specific MIME type (Ex: 'image', 'image/png', 'application/pdf')
+ *     @param {Int} [render_options.library.uploadedTo]  Only display items
+ *     uploaded to a specific post ID
+ *     @param {String} [render_options.library.orderby='date']  Set the orderby
+ *     for the items displayed
+ *     @param {String} [render_options.library.order='DESC']  Set the order of
+ *     the items displayed
  *
  ***/
 var MediaModal = function (settings, render_options) {
@@ -102,6 +119,8 @@ var MediaModal = function (settings, render_options) {
     // Create the media frame.
     frame = wp.media.frames.file_frame = wp.media(
       jQuery.extend(true, {
+		// Default values for rendering modal window (defined here so that
+		// they're not parsed before the even fires)
         title: jQuery(this).data('uploader_title'),
         button: {
           text: jQuery(this).data('uploader_button_text')
@@ -128,7 +147,7 @@ var MediaModal = function (settings, render_options) {
       // Get the link/button/etc that called us
       var $caller = jQuery(that.settings.calling_selector);
 
-      // Select the attachment if we have one
+      // Select the attachments if we have any
       if ($caller.data('attachment_ids')) {        
         var Attachment = wp.media.model.Attachment;
         var selection = frame.state().get('selection');
